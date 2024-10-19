d02dff9598e7ad6876b83b99f6d3480afad1d76a
Cacanuck <156028686+Cacanuck@users.noreply.github.com>
2024-10-19 11:41:30 -0400

    Added face recognition

---
+++ Project2/SelfieApp/main.py
@@ -0,0 +1,31 @@
+#To run: In Terminal, go to CS4900/Project2 folder, and type in `python main.py`
+
+import face_recognition
+import cv2
+import numpy as np
+
+video_capture = cv2.VideoCapture(0)
+
+face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+def detect_bounding_box(vid):
+    gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 4))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    return faces
+    
+while True:
+
+    result, video_frame = video_capture.read()  
+    if result is False:
+        break  
+
+    faces = detect_bounding_box(video_frame)
+    cv2.imshow("", video_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
