ddf268dd4d87ff1f32b4ea2fd6280b856a503f1a
Cacanuck <156028686+Cacanuck@users.noreply.github.com>
2024-10-21 17:23:18 -0400

    added head up and head down tts

--- Project2/SelfieApp/main.py
+++ Project2/SelfieApp/main.py
@@ -8,14 +8,6 @@ import numpy as np
 from gtts import gTTS
 import pygame
 
-myText = 'Keep Still'
-language = 'en'
-myobj = gTTS(text=myText, lang=language, slow=False)
-myobj.save("keepStill.mp3")
-pygame.mixer.int()
-pygame.mixer.music.load("keepStill")
-pygame.mixer.music.play()
-
 video_capture = cv2.VideoCapture(0)
 
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
